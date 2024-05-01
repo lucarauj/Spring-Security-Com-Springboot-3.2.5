6eb1f4056dc55eea74346007319c2d82b1c378f3
lucarauj <lukas.martins193@gmail.com>
2024-05-01 17:25:58 -0300

    feat: adiciona Auth Database

--- access-control/src/main/java/izacontent/accesscontrol/security/WebSecurityConfig.java
+++ access-control/src/main/java/izacontent/accesscontrol/security/WebSecurityConfig.java
@@ -1,33 +1,24 @@
 package izacontent.accesscontrol.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig {
 
-    @Bean
-    public UserDetailsService userDetailsService() {
-        UserDetails user = User.withUsername("user")
-                .password("{noop}user123")
-                .roles("USERS")
-                .build();
-
-        UserDetails admin = User.withUsername("admin")
-                .password("{noop}master123")
-                .roles("MANAGERS")
-                .build();
-
-        return new InMemoryUserDetailsManager(user, admin);
+    @Autowired
+    private SecurityDatabaseService securityService;
+    @Autowired
+    public void globalUserDetails(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(securityService).passwordEncoder(NoOpPasswordEncoder.getInstance());
     }
 
     @Bean
@@ -39,7 +30,9 @@ public class WebSecurityConfig {
                         .requestMatchers("/managers").hasRole("MANAGERS")
                         .requestMatchers("/users").hasAnyRole("USERS","MANAGERS")
                         .anyRequest().authenticated())
-                        .httpBasic(Customizer.withDefaults());
+                        .httpBasic(Customizer.withDefaults())
+                        .formLogin(Customizer.withDefaults());
+
 
         return http.build();
     }
